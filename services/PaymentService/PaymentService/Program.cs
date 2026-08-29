b76c68afa8a1f0bd302640628163728043fcb9f7
﻿Sandesh <trymesan204@gmail.com>
2026-08-29 11:38:15 +0545

    adding docker support

--- services/PaymentService/PaymentService/Program.cs
+++ services/PaymentService/PaymentService/Program.cs
@@ -19,11 +19,11 @@ builder.Services.AddScoped<IPaymentService, PaymentsService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
