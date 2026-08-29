44a93e4e403600dcda84301aef2f6b0a66618fb5
Sandesh Upadhyaya <sandesh@mutualart.com>
2026-08-29 11:38:15 +0545

    adding docker support

--- services/PaymentService/PaymentService.Domain/Enums/PaymentStatus.cs
+++ services/PaymentService/PaymentService.Domain/Enums/PaymentStatus.cs
@@ -4,5 +4,6 @@ public enum PaymentStatus
 {
     Pending,
     Succeeded,
-    Failed
+    Failed,
+    Timeout
 }
--- services/PaymentService/PaymentService.Infrastructure/Services/MockPaymentProcessor.cs
+++ services/PaymentService/PaymentService.Infrastructure/Services/MockPaymentProcessor.cs
@@ -7,9 +7,15 @@ public class MockPaymentProcessor : IPaymentProcessor
 {
     public Task<PaymentStatus> ProcessPaymentAsync(decimal amount, string currency, CancellationToken cancellationToken = default)
     {
-        var status = Random.Shared.Next(2) == 0
-            ? PaymentStatus.Succeeded
-            : PaymentStatus.Failed;
+        int[] failed = [5, 10, 15];
+        int timeout = 0;
+        var randomStatus = Random.Shared.Next(20);
+        var status = randomStatus == timeout 
+        ? PaymentStatus.Timeout 
+        : (failed.Contains(randomStatus)
+            ? PaymentStatus.Failed
+            : PaymentStatus.Succeeded
+        );
 
         return Task.FromResult(status);
     }
